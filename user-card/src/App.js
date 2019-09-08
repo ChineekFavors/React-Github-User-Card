ab95075a91045014105f6811e037563bc1f55496
Chineek Favors <chineekfavors01@gmail.com>
2019-09-07 23:52:09 -0700

    created Card component and pass props from App.js

--- user-card/src/App.js
+++ user-card/src/App.js
@@ -2,18 +2,21 @@ import React from 'react';
 
 import './App.css';
 
+import Card from "./components/card/Card";
+
 class App extends React.Component{
   constructor(){
     super();
     this.state = {
-      user:[]
+      user:[],
     };
   }
 
   componentDidMount(){
     fetch('https://api.github.com/users/chineekfavors')
       .then(res => res.json())
-      .then(data => console.log(data))
+      .then(data => this.setState({user: data }))
+      .then(() => console.log(this.state.user))
       .catch(error => console.log('there was a problem', error))
   };
 
@@ -21,6 +24,7 @@ class App extends React.Component{
     return (
       <div className="App">
       <h1>Github-User-Card</h1>
+      <Card user={this.state.user}/>
         
       </div>
     );
---
+++ user-card/src/components/card/Card.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+ const Card = props => {
+	console.log(props.user)
+
+
+	
+		return (
+			<div>
+			from the card
+			</div>
+		);
+	
+};
+export default Card;
