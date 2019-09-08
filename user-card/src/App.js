008a337807d7485eb5ee55d9ea7b4bab6e08ff99
Chineek Favors <chineekfavors01@gmail.com>
2019-09-07 21:05:17 -0700

    initial start created react-app

---
+++ user-card/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import './App.css';
+
+class App extends React.Component{
+  constructor(){
+    super();
+  }
+  render(){
+    return (
+      <div className="App">
+      <h1>Github-User-Card</h1>
+        
+      </div>
+    );
+  }
+}
+
+export default App;
